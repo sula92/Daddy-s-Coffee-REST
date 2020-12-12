d2f34be097a80705cb598e7e78b4a12d824b9562
sulakkhanaid <sulakkanaid@gmail.com>
2020-12-12 15:59:25 +0530

    initial commit

---
+++ src/main/java/com/sula/coffeeshop/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.sula.coffeeshop.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+
+    @RequestMapping("/")
+    public String getHelloWorld(){
+        return "Hello World";
+    }
+
+
+}
