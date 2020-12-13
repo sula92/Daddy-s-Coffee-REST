9432b39a2b0581ac7d64608418bf8083d9097088
sulakkhanaid <sulakkanaid@gmail.com>
2020-12-13 16:24:03 +0530

    adding query to searchorderrepo

--- src/main/java/com/sula/coffeeshop/controller/OrderController.java
+++ src/main/java/com/sula/coffeeshop/controller/OrderController.java
@@ -14,6 +14,7 @@ import org.springframework.web.server.ResponseStatusException;
 import javax.validation.Valid;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
--- src/main/java/com/sula/coffeeshop/repository/custom/OrderRepository.java
+++ src/main/java/com/sula/coffeeshop/repository/custom/OrderRepository.java
@@ -4,12 +4,15 @@ import com.sula.coffeeshop.dto.OrderDTO;
 import com.sula.coffeeshop.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import javax.management.loading.MLetContent;
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order,String> {
     public Order getFirstLastOrderIdByOrderByIdDesc();
-    @Query("")
-    public List<OrderDTO> searchOrder();
+
+    @Query("select O from Order O where O.id LIKE '%:oid%'")
+    public List<Order> searchOrder(@Param("oid") String oid);
 }
